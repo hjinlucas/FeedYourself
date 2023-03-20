d4f86f6687789c34430b24874b8a4b738134b94b
lucasjin <hjin.lucas@gmail.com>
2023-03-20 17:11:33 -0400

    Create the adapter to display images and handle click events

--- app/src/main/java/com/example/feedyourself/ImageAdapter.java
+++ app/src/main/java/com/example/feedyourself/ImageAdapter.java
@@ -1,4 +1,68 @@
 package com.example.feedyourself;
 
-public class ImageAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    //create the adapter to display images and handle click events
+
+    private List<Integer> imageIds;
+    private Context context;
+    private OnItemClickListener onItemClickListener;
+
+    public ImageAdapter(Context context, List<Integer> imageIds) {
+        this.context = context;
+        this.imageIds = imageIds;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_card_view, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        holder.cardImageView.setImageResource(imageIds.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageIds.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView cardImageView;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardImageView = itemView.findViewById(R.id.cardImageView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(getAdapterPosition());
+            }
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
 }
+
