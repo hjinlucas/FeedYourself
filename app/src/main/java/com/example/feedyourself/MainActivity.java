3939450e480e1d3000cc8f48727c8c7443a85796
lucasjin <hjin.lucas@gmail.com>
2023-03-20 17:18:06 -0400

    Update MainActivity for cards onClick

--- app/src/main/java/com/example/feedyourself/MainActivity.java
+++ app/src/main/java/com/example/feedyourself/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.feedyourself;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //==================================================================================================
+        //============= OnClickListener for small menu in main page ========================================
+        //==================================================================================================
         forYouTextView = findViewById(R.id.forYouTextView);
         exploreTextView = findViewById(R.id.exploreTextView);
         premiumTextView = findViewById(R.id.premiumTextView);
@@ -40,6 +46,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Handle click event for Premium TextView
+            }
+        });
+
+        //==================================================================================================
+        //============= OnClickListener for cards that display image of breakfast, brunch, etc =============
+        //==================================================================================================
+        RecyclerView horizontalRecyclerView = findViewById(R.id.horizontalRecyclerView);
+
+        List<Integer> imageIds = Arrays.asList(
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background
+                //just for test now, replace image later
+        );
+
+        ImageAdapter imageAdapter = new ImageAdapter(this, imageIds);
+        horizontalRecyclerView.setAdapter(imageAdapter);
+
+        imageAdapter.setOnItemClickListener(new ImageAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                // Handle click event for the ImageView in the CardView
+                // You can get the selected image ID with imageIds.get(position)
             }
         });
     }
