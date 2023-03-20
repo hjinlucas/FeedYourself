c4cff9eba8d85fcf46d54a7034a98875bbcbff0c
lucasjin <hjin.lucas@gmail.com>
2023-03-20 18:21:03 -0400

    Update confirmation btn visibility

--- app/src/main/java/com/example/feedyourself/MainActivity.java
+++ app/src/main/java/com/example/feedyourself/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.feedyourself;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.Arrays;
 import java.util.List;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView forYouTextView;
     private TextView exploreTextView;
     private TextView premiumTextView;
+
+    private TextView breakfastTextView;
+    private TextView brunchTextView;
+    private TextView lunchTextView;
+    private TextView dinnerTextView;
+    private FloatingActionButton confirmationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +58,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         //==================================================================================================
-        //============= OnClickListener for cards that display image of breakfast, brunch, etc =============
+        //============= OnClickListener for cards that display meal recommendations =======================
         //==================================================================================================
         RecyclerView horizontalRecyclerView = findViewById(R.id.horizontalRecyclerView);
 
@@ -72,6 +82,42 @@ public class MainActivity extends AppCompatActivity {
                 // You can get the selected image ID with imageIds.get(position)
             }
         });
+
+
+        //==================================================================================================
+        //============= OnClickListener for cards that display image of breakfast, brunch, etc =============
+        //==================================================================================================
+        // Add this code inside the onCreate method, after initializing the horizontal RecyclerView
+
+        breakfastTextView = findViewById(R.id.breakfastTextView);
+        brunchTextView = findViewById(R.id.brunchTextView);
+        lunchTextView = findViewById(R.id.lunchTextView);
+        dinnerTextView = findViewById(R.id.dinnerTextView);
+        confirmationButton = findViewById(R.id.confirmationButton);
+
+        View.OnClickListener mealTypeClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Perform any action you want when a meal type is selected
+
+                // Show the confirmation button when a meal type is selected
+                confirmationButton.setVisibility(View.VISIBLE);
+            }
+        };
+
+        breakfastTextView.setOnClickListener(mealTypeClickListener);
+        brunchTextView.setOnClickListener(mealTypeClickListener);
+        lunchTextView.setOnClickListener(mealTypeClickListener);
+        dinnerTextView.setOnClickListener(mealTypeClickListener);
+
+        confirmationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle click event for the confirmation button
+                // For example, navigate to the next page with the selected meal type
+            }
+        });
+
     }
 }
 
