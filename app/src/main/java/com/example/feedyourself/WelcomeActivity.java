e20a44ad754962ec89ad8eef514c1ab7e3394c00
lucasjin <hjin.lucas@gmail.com>
2023-03-20 20:59:50 -0400

    1. Create welcome page for app

---
+++ app/src/main/java/com/example/feedyourself/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.feedyourself;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // Set the duration for the welcome page (e.g., 2000 milliseconds = 2 seconds)
+        int welcomePageDuration = 1200;
+
+        new Handler().postDelayed(() -> {
+            // Start the main activity after the specified duration
+            Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            finish();
+        }, welcomePageDuration);
+    }
+}
+
