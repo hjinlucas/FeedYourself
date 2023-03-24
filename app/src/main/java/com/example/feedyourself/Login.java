033c3ddea2280f55e3b2fac67a253ed66a8d7792
lucasjin <hjin.lucas@gmail.com>
2023-03-24 15:26:23 -0400

    Linked firebase db& Create login & register activities

---
+++ app/src/main/java/com/example/feedyourself/Login.java
@@ -0,0 +1,14 @@
+package com.example.feedyourself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
