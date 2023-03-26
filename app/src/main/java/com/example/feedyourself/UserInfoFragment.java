aa9417cb2b19435fdf0b4057b5f95d34ae146744
lucasjin <hjin.lucas@gmail.com>
2023-03-26 14:06:47 -0400

    User Info Fragment Created

---
+++ app/src/main/java/com/example/feedyourself/UserInfoFragment.java
@@ -0,0 +1,24 @@
+package com.example.feedyourself;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class UserInfoFragment extends Fragment {
+
+    public UserInfoFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_info, container, false);
+
+        return view;
+    }
+
+}
