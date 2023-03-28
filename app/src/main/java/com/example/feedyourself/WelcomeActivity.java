67a92998ddf68ee47d781da0844a2bb52457cb92
lucasjin <hjin.lucas@gmail.com>
2023-03-27 22:39:47 -0400

    Create Animation for Welcome Page

--- app/src/main/java/com/example/feedyourself/WelcomeActivity.java
+++ app/src/main/java/com/example/feedyourself/WelcomeActivity.java
@@ -3,18 +3,28 @@ package com.example.feedyourself;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    Animation topAnim;
+    ImageView imageView;
+    TextView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
 
         // Set the duration for the welcome page (e.g., 2000 milliseconds = 2 seconds)
-        int welcomePageDuration = 1200;
+        int welcomePageDuration = 1600;
 
         new Handler().postDelayed(() -> {
             // Start the main activity after the specified duration
@@ -22,6 +32,14 @@ public class WelcomeActivity extends AppCompatActivity {
             startActivity(mainIntent);
             finish();
         }, welcomePageDuration);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+
+        //Hooks
+        imageView = findViewById(R.id.welcome_image);
+
+        imageView.setAnimation(topAnim);
     }
 
 }
