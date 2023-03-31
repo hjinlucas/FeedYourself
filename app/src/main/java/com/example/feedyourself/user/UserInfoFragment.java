73aff1a29679757c33540f09f4fe74ccf671966e
SYL <zoezhang94@gmail.com>
2023-03-31 10:21:46 -0400

    let user choose pic from gallery for profile image

--- app/src/main/java/com/example/feedyourself/user/UserInfoFragment.java
+++ app/src/main/java/com/example/feedyourself/user/UserInfoFragment.java
@@ -1,7 +1,12 @@
 package com.example.feedyourself.user;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +18,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.feedyourself.R;
 import com.example.feedyourself.databinding.FragmentUserInfoBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -20,21 +28,28 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserInfoFragment extends Fragment {
-    private static final int GALLERY_REQUEST_CODE = 1;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     private FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
     private Button Logout;
+    private ImageView profileImage;
+    private StorageReference storageReference;
+    private FirebaseFirestore fireStore;
     private FragmentUserInfoBinding binding;
     GoogleSignInOptions gso;
-    private ImageView userImageView;
-
-    private SharedViewModel sharedViewModel;
-    public UserInfoFragment(){
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,26 +60,21 @@ public class UserInfoFragment extends Fragment {
                 .requestIdToken("596346761447-togmbri8f6169ivhlmumoop3iqo3jg6v.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
+        // Initialize the firestore variable before calling fetchUserInfo()
+        fireStore = FirebaseFirestore.getInstance();
         fetchUserInfo();
-        userImageView = binding.getRoot().findViewById(R.id.profile_image);
-        userImageView.setOnClickListener(this::showPopupMenu);
 
-        return binding.getRoot();
-    }
+        profileImage = binding.profileImage;
+        storageReference = FirebaseStorage.getInstance().getReference("profile_images");
 
-    private void showPopupMenu(View view) {
-        PopupMenu popupMenu = new PopupMenu(requireContext(), view);
-        popupMenu.inflate(R.menu.popup_menu);
-        popupMenu.setOnMenuItemClickListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.profile_image:
-                    openGallery();
-                    return true;
-                default:
-                    return false;
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
             }
         });
-        popupMenu.show();
+
+        return binding.getRoot();
     }
 
     @Override
@@ -73,35 +83,125 @@ public class UserInfoFragment extends Fragment {
 
         mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
         binding.logoutButton.setOnClickListener(V -> {
-
-
             logOut();
         });
-
-
-
     }
 
-    private void openGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, GALLERY_REQUEST_CODE);
-    }
-    private void logOut(){
+    private void logOut() {
         mAuth.signOut();
         mGoogleSignInClient.signOut();
         FragmentLogin login = new FragmentLogin();
         getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, login).commit();
-
-
     }
-    private void fetchUserInfo(){
+
+    private void fetchUserInfo() {
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        if (currentUser != null) {
             String displayName = currentUser.getDisplayName();
             String email = currentUser.getEmail();
             binding.profileEmail.setText("Email: " + email);
             binding.profileName.setText("Name: " + displayName);
+
+            // Load the user's profile image
+            String userId = currentUser.getUid();
+            fireStore.collection("users").document(userId).get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            String imageUrl = documentSnapshot.getString("profileImageUrl");
+                            if (imageUrl != null) {
+                                // Load the image with a placeholder and/or an image loading animation
+                                RequestOptions requestOptions = new RequestOptions()
+                                        .placeholder(R.drawable.default_image) // Replace with your default profile image drawable resource
+                                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                                        .centerCrop();
+
+                                Glide.with(requireContext())
+                                        .load(imageUrl)
+                                        .apply(requestOptions)
+                                        .into(profileImage);
+                            }
+                        }
+                    });
         }
     }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(getContext(), view);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.choose_from_gallery:
+                    openGallery();
+                    return true;
+                default:
+                    return false;
+            }
+        });
+
+        popupMenu.show();
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            Uri imageUri = null;
+            if (requestCode == PICK_IMAGE_REQUEST && data != null && data.getData() != null) {
+                imageUri = data.getData();
+            }
+            if (imageUri != null) {
+                updateProfileImage(imageUri);
+            }
+        }
+    }
+
+    private Uri getImageUri(Context context, Bitmap imageBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), imageBitmap, "Title", null);
+        return Uri.parse(path);
+    }
+
+    private void updateProfileImage(Uri imageUri) {
+        // Update the ImageView with the new image
+        Glide.with(requireContext())
+                .load(imageUri)
+                .centerCrop()
+                .into(profileImage);
+
+        // Save the new image to Firebase Storage
+        String userId = mAuth.getCurrentUser().getUid();
+        StorageReference imageRef = storageReference.child(userId + ".jpg");
+        imageRef.putFile(imageUri).addOnSuccessListener(taskSnapshot -> {
+            // Get the download URL and update the user's profile image URL in Firebase Firestore
+            imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                String imageUrl = uri.toString();
+                updateUserProfileImageUrl(userId, imageUrl);
+            });
+        });
+    }
+
+    private void updateUserProfileImageUrl(String userId, String imageUrl) {
+        DocumentReference userDocRef = fireStore.collection("users").document(userId);
+        Map<String, Object> data = new HashMap<>();
+        data.put("profileImageUrl", imageUrl);
+
+        userDocRef.set(data, SetOptions.merge())
+                .addOnSuccessListener(aVoid -> {
+                    // Profile image URL updated successfully
+                })
+                .addOnFailureListener(e -> {
+                    // Error updating profile image URL
+                });
+    }
 }
+
